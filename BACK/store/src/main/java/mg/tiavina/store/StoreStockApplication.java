9c068a5f3993843747bf556a5e93e0a803da231f
TiavinaMalalaniaina <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2023-11-09 22:28:16 +0300

    back initialize

---
+++ BACK/store/src/main/java/mg/tiavina/store/StoreStockApplication.java
@@ -0,0 +1,13 @@
+package mg.tiavina.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreStockApplication.class, args);
+	}
+
+}
